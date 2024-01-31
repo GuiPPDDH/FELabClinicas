d0ac0a840a2d88d0755f2a5670c2cb349769005f
Guilherme Potter Petry <guipotterrp@gmail.com>
2024-01-31 11:07:39 -0300

    Criado lab_clinicas_core_config

---
+++ fe_lab_clinicas_core/lib/src/lab_clinicas_core_config.dart
@@ -0,0 +1,53 @@
+import 'package:asyncstate/asyncstate.dart';
+import 'package:fe_lab_clinicas_core/src/loader/lab_clinicas_loader.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class LabClinicasCoreConfig extends StatelessWidget {
+  final ApplicationBindings? applicationBindings;
+  final List<FlutterGetItPageRouter>? pages;
+  final List<FlutterGetItPageBuilder>? pagesBuilders;
+  final List<FlutterGetItModule>? modules;
+  final String title;
+
+  const LabClinicasCoreConfig({
+    super.key,
+    this.applicationBindings,
+    this.pages,
+    this.pagesBuilders,
+    this.modules,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterGetIt(
+      debugMode: kDebugMode,
+      bindings: applicationBindings,
+      bindingsBuilder: () => [
+        Bind.lazySingleton((i) => ''),
+      ],
+      pages: [
+        ...pages ?? [],
+        ...pagesBuilders ?? [],
+      ],
+      modules: modules,
+      builder: (context, routes, flutterGetItNavObserver) {
+        return AsyncStateBuilder(
+          loader: LabClinicasLoader(),
+          builder: (navigatorObserver) {
+            return MaterialApp(
+              title: title,
+              navigatorObservers: [
+                navigatorObserver,
+                flutterGetItNavObserver,
+              ],
+              routes: routes,
+            );
+          },
+        );
+      },
+    );
+  }
+}
