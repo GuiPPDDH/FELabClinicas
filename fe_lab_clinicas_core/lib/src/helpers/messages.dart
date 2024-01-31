e9f91383073525172f221a2c63ba775adac1f170
Guilherme Potter Petry <guipotterrp@gmail.com>
2024-01-31 11:27:08 -0300

    Criado MessageStateMixin, e MessagesViewMixin

--- fe_lab_clinicas_core/lib/src/helpers/messages.dart
+++ fe_lab_clinicas_core/lib/src/helpers/messages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
@@ -22,5 +23,58 @@ final class Messages {
       Overlay.of(context),
       CustomSnackBar.error(message: message),
     );
+  }
+}
+
+mixin MessageStateMixin {
+  final Signal<String?> _infoMessage = signal(null);
+  String? get infoMessage => _infoMessage();
+
+  final Signal<String?> _successMessage = signal(null);
+  String? get successMessage => _successMessage();
+
+  final Signal<String?> _errorMessage = signal(null);
+  String? get errorMessage => _errorMessage();
+
+  void clearInfo() => _infoMessage.value = null;
+  void clearSuccess() => _successMessage.value = null;
+  void clearError() => _errorMessage.value = null;
+
+  void showInfo(String message) {
+    untracked(() => clearInfo());
+    _infoMessage.value = message;
+  }
+
+  void showSuccess(String message) {
+    untracked(() => clearSuccess());
+    _successMessage.value = message;
+  }
+
+  void showError(String message) {
+    untracked(() => clearError());
+    _errorMessage.value = message;
+  }
+
+  void clearAllMessages() {
+    untracked(() {
+      clearInfo();
+      clearSuccess();
+      clearError();
+    });
+  }
+}
+
+mixin MessageViewMixin<T extends StatefulWidget> on State<T> {
+  void messageListener(MessageStateMixin messageStateMixin) {
+    effect(() {
+      switch (messageStateMixin) {
+        case MessageStateMixin(:final infoMessage?):
+          Messages.showInfo(infoMessage, context);
+        case MessageStateMixin(:final successMessage?):
+          Messages.showSuccess(successMessage, context);
+        case MessageStateMixin(:final errorMessage?):
+          Messages.showSuccess(errorMessage, context);
+      }
+    });
   }
 }
