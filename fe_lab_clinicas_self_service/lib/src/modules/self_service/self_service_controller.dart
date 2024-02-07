dadc4897f82c0e6a3200860e34ee768593c65b2f
Guilherme Potter Petry(GuiDDH) <guipotterrp@gmail.com>
2024-02-07 10:50:17 -0300

    Realizado implementações em self_service controller, module e page

--- fe_lab_clinicas_self_service/lib/src/modules/self_service/self_service_controller.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/self_service_controller.dart
@@ -1,4 +1,21 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 
-class SelfServiceController {
-  
+enum FormSteps {
+  none,
+  whoIAm,
+  findPatient,
+  patient,
+  documents,
+  done,
+  restart,
+}
+
+class SelfServiceController with MessageStateMixin {
+  final _steps = ValueSignal(FormSteps.none);
+  FormSteps get step => _steps();
+
+  void startProcess() {
+    _steps.forceUpdate(FormSteps.whoIAm);
+  }
 }
