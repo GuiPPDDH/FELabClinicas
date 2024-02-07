5776acaf09675b7e4609782d447cabafb9956d90
Guilherme Potter Petry(GuiDDH) <guipotterrp@gmail.com>
2024-02-07 09:55:48 -0300

    Criado self_service

---
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/documents/scan_confirm/scan_confirm_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ScanConfirmPage extends StatelessWidget {
+
+  const ScanConfirmPage({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Scan Confirm'),),
+           body: Container(),
+       );
+  }
+}
---
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/self_service_module.dart
@@ -0,0 +1,26 @@
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/documents/documents_page.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/documents/scan/scan_page.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/done/done_page.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/find_patient/find_patient_page.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/patient/patient_page.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/self_service_page.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/who_i_am/who_i_am_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class SelfServiceModule extends FlutterGetItModule {
+  @override
+  String get moduleRouteName => '/self-service';
+
+  @override
+  Map<String, WidgetBuilder> get pages => {
+    '/': (context) => const SelfServicePage(),
+    '/who-i-am': (context) => const WhoIAmPage(),
+    '/find-patient': (context) => const FindPatientPage(),
+    '/patient': (context) => const PatientPage(),
+    '/documents': (context) => const DocumentsPage(),
+    '/documents/scan': (context) => const ScanPage(),
+    '/documents/scan/confirm': (context) => const ScanPage(),
+    '/done':(context) => const DonePage(),
+  };
+}
