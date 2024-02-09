2599854fc99b365c9728efc2e93e339a73c7a277
Guilherme Potter Petry(GuiDDH) <guipotterrp@gmail.com>
2024-02-09 08:25:11 -0300

    Criado patient_repository_impl

--- fe_lab_clinicas_self_service/lib/src/modules/self_service/self_service_module.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/self_service_module.dart
@@ -1,7 +1,6 @@
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/documents/documents_page.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/documents/scan/scan_page.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/done/done_page.dart';
-import 'package:fe_lab_clinicas_self_service/src/modules/self_service/find_patient/find_patient_page.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/find_patient/find_patient_router.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/patient/patient_page.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/self_service_controller.dart';
@@ -11,24 +10,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
 class SelfServiceModule extends FlutterGetItModule {
-
   @override
   List<Bind<Object>> get bindings => [
-    Bind.lazySingleton((i) => SelfServiceController()),
-  ];
+        Bind.lazySingleton((i) => SelfServiceController()),
+      ];
 
   @override
   String get moduleRouteName => '/self-service';
 
   @override
   Map<String, WidgetBuilder> get pages => {
-    '/': (context) => const SelfServicePage(),
-    '/who-i-am': (context) => const WhoIAmPage(),
-    '/find-patient': (context) => const FindPatientRouter(),
-    '/patient': (context) => const PatientPage(),
-    '/documents': (context) => const DocumentsPage(),
-    '/documents/scan': (context) => const ScanPage(),
-    '/documents/scan/confirm': (context) => const ScanPage(),
-    '/done':(context) => const DonePage(),
-  };
+        '/': (context) => const SelfServicePage(),
+        '/who-i-am': (context) => const WhoIAmPage(),
+        '/find-patient': (context) => const FindPatientRouter(),
+        '/patient': (context) => const PatientPage(),
+        '/documents': (context) => const DocumentsPage(),
+        '/documents/scan': (context) => const ScanPage(),
+        '/documents/scan/confirm': (context) => const ScanPage(),
+        '/done': (context) => const DonePage(),
+      };
 }
--- fe_lab_clinicas_self_service/lib/src/repositories/patient/patient_repository.dart
+++ fe_lab_clinicas_self_service/lib/src/repositories/patient/patient_repository.dart
@@ -2,5 +2,5 @@ import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:fe_lab_clinicas_self_service/src/model/patient_model.dart';
 
 abstract interface class PatientRepository {
-  Future<Either<RepositoryException, PatientModel>> findPatientByCPF(String cpf);
+  Future<Either<RepositoryException, PatientModel?>> findPatientByCPF(String document);
 }
