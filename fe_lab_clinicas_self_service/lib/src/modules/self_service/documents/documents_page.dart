2d86c9c456769bdd2e4db9473fc93e1b0f5c9a23
Guilherme Potter Petry(GuiDDH) <guipotterrp@gmail.com>
2024-02-11 16:17:07 -0300

    Criado a tela DocumentsPage e o Widget DocumentBoxWidget

--- fe_lab_clinicas_self_service/lib/src/modules/self_service/documents/documents_page.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/documents/documents_page.dart
@@ -1,14 +1,113 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/documents/widgets/document_box_widget.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/widgets/lab_clinicas_self_service_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class DocumentsPage extends StatelessWidget {
+  const DocumentsPage({super.key});
 
-  const DocumentsPage({ super.key });
+  @override
+  Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+    return Scaffold(
+      appBar: LabClinicasSelfServiceAppBar(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Container(
+            width: sizeOf.width * 0.85,
+            margin: const EdgeInsets.only(top: 18),
+            padding: const EdgeInsets.all(32),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(
+                color: LabClinicasTheme.orangeColor,
+              ),
+            ),
+            child: Column(
+              children: [
+                Image.asset('assets/images/folder.png'),
+                const SizedBox(
+                  height: 24,
+                ),
+                const Text(
+                  'ADICIONAR DOCUMENTOS',
+                  style: LabClinicasTheme.titleSmallStyle,
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                const Text(
+                  'Selecione o documento que deseja fotografar',
+                  style: TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w500, color: LabClinicasTheme.blueColor),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                SizedBox(
+                  width: sizeOf.width * 0.8,
+                  height: 300,
+                  child: Row(
+                    children: [
+                      DocumentBoxWidget(
+                        uploaded: false,
+                        icon: Image.asset('assets/images/id_card.png'),
+                        label: 'CARTEIRINHA',
+                        totalFiles: 1,
+                      ),
+                      const SizedBox(
+                        width: 24,
+                      ),
+                      DocumentBoxWidget(
+                        uploaded: false,
+                        icon: Image.asset('assets/images/document.png'),
+                        label: 'PEDIDO MÉDICO',
+                        totalFiles: 1,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: Colors.red,
+                          side: const BorderSide(
+                            color: Colors.red,
+                          ),
+                          fixedSize: const Size.fromHeight(48),
+                        ),
+                        child: Text('REMOVER TODAS'),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: LabClinicasTheme.orangeColor,
+                          fixedSize: const Size.fromHeight(48),
+                        ),
+                        child: Text('FINALIZAR'),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
